a48d47be0cad8210c4734719c23e1eb9ee6bf687
tracer-ics2015 <tracer@njuics.org>
2015-10-04 21:47:52 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:47:52 up  1:15,  1 user,  load average: 0.04, 0.04, 0.05 26b5e7f68470e6671ecfa24eb5637a56a9d92bd4

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -83,7 +83,7 @@ static int cmd_x(char *args)
  //  printf("%u\n",add);*/
    int i1;
   for (i1=0;i1<n;i1++)
-   printf("%d ",swaddr_read(add,n));
+   printf("%d ",swaddr_read(add,3));
    return 0;
 }
 
