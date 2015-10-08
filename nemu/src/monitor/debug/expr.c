3ccbcddda1f5f0a3669289e3a4814866d111f8f1
tracer-ics2015 <tracer@njuics.org>
2015-10-08 21:23:35 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:23:35 up 49 min,  1 user,  load average: 0.24, 0.16, 0.15 b2545e39245af243877aed734a7575251ab09e74

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -226,7 +226,7 @@ uint32_t expr(char *e, bool *success) {
 	int i1;
 	for ( i1=0;i1<nr_token;i1++)
 		printf("%d\n",tokens[i1].type);
-	*success=true;
+//	*success=true;
 		//	return 100000;
 	return  eval(0,nr_token-1);
 
