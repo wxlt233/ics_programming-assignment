08f59b640a210001ff5b58ce10e8827fd93e11e0
tracer-ics2015 <tracer@njuics.org>
2015-11-18 17:10:31 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:10:31 up  2:44,  3 users,  load average: 0.00, 0.01, 0.05 49a750f212cb4bf69f42be3f2b1166a358488ec7

---
+++ nemu/src/cpu/exec/data-mov/lea-template.h
@@ -0,0 +1,14 @@
+/*#include "cpu/exec/template-start.h"
+
+#define instr lea
+
+static void do_execute() {
+	OPERAND_W(op_dest, op_src->addr);
+	print_asm_template2();
+}
+
+
+make_instr_helper(m2r)
+
+#include "cpu/exec/template-end.h"
+*/
---
+++ nemu/src/cpu/exec/data-mov/lea.h
@@ -0,0 +1,8 @@
+/*#ifndef __LEA_H__
+#define __LEA_H__
+
+
+make_helper(lea_m2r_v);
+
+#endif
+*/
