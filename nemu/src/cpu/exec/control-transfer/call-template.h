e7e5947372a2396a9ff3f5bdfdd233506b425de8
tracer-ics2015 <tracer@njuics.org>
2015-11-18 10:09:44 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:09:44 up  1:22,  3 users,  load average: 0.01, 0.02, 0.05 61d03898699700cea3337d6f16793e528698eb1

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	cpu.esp-=4;
 	swaddr_write(cpu.esp,4,cpu.eip);
 	printf("%x  %x ",cpu.eip,op_src->val);
-	cpu.eip+=5+op_src->val;
+	cpu.eip+=+op_src->val;
 }
 
 make_instr_helper(i)
