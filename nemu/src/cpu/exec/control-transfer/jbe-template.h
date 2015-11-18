19496a2946451b076d7e3af82f2d5e87d0325b38
tracer-ics2015 <tracer@njuics.org>
2015-11-18 15:34:59 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:34:59 up  1:08,  3 users,  load average: 0.08, 0.03, 0.05 cc80e5479a69204f34890a7261f0f88bb43d8d77

--- nemu/src/cpu/exec/control-transfer/jbe-template.h
+++ nemu/src/cpu/exec/control-transfer/jbe-template.h
@@ -4,9 +4,12 @@
 
 static void do_execute() {
 //	printf("%o",op_src->val&0x000000ff);
- if (DATA_BYTE==1){	int t=op_src->val&0x000000ff;
+ if (DATA_BYTE==1)
+ {
+ 	 char t=op_src->val&0x000000ff;
 	 printf("%d",t);
-	if (cpu.eflags.ZF==1||cpu.eflags.CF==1) cpu.eip+=t;}
+	if (cpu.eflags.ZF==1||cpu.eflags.CF==1) cpu.eip+=t;
+ }
 	//	(int  )(op_src->val&0xff);
 	print_asm_template1();
 }
--- nemu/src/cpu/exec/control-transfer/je-template.h
+++ nemu/src/cpu/exec/control-transfer/je-template.h
@@ -3,7 +3,12 @@
 #define instr je
 
 static void do_execute() {
-	if (cpu.eflags.ZF==1) cpu.eip+=op_src->val;
+	//if (cpu.eflags.ZF==1) cpu.eip+=op_src->val;
+ if (DATA_BYTE==1)
+ {
+ 	 char t=op_src->val&0x000000ff;
+	if (cpu.eflags.ZF==1) cpu.eip+=t;
+ }
 	print_asm_template1();
 }
 
