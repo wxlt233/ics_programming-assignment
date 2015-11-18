357267b73af713abd0532bd81e96d7ed4270cb89
tracer-ics2015 <tracer@njuics.org>
2015-11-18 09:50:52 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  09:50:52 up  1:03,  3 users,  load average: 0.00, 0.01, 0.05 d7226f6b11887ceb4386356c24d9bf4adf0aeef7

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -2,7 +2,7 @@
 #define instr sub
 
 static void do_execute() {
-	printf("doing sub");
+//	printf("doing sub");
 	DATA_TYPE result=op_dest->val-op_src->val;
     DATA_TYPE a=op_dest->val;
 	DATA_TYPE b=op_src->val;	
