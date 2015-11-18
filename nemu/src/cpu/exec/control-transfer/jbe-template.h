3f6d2bfefa1136366da803c84a6b74241ab3bbff
tracer-ics2015 <tracer@njuics.org>
2015-11-18 15:08:04 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:08:04 up 41 min,  3 users,  load average: 0.00, 0.03, 0.05 201ef700bae62e2fb8f69fbd7ac760521f1c81c1

--- nemu/src/cpu/exec/control-transfer/jbe-template.h
+++ nemu/src/cpu/exec/control-transfer/jbe-template.h
@@ -3,7 +3,7 @@
 #define instr jbe
 
 static void do_execute() {
-	if (cpu.eflags.ZF==1||cpu.eflags.CF==1) cpu.eip+=op_src->val;
+	if (cpu.eflags.ZF==1||cpu.eflags.CF==1) cpu.eip+=(int)(op_src->val);
 	print_asm_template1();
 }
 
