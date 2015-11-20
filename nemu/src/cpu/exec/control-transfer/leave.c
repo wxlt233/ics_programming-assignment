9429de68b63bdafa212ab554b7408af0f36ca670
tracer-ics2015 <tracer@njuics.org>
2015-11-20 16:01:44 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:01:44 up 51 min,  3 users,  load average: 0.00, 0.01, 0.05 a0f8c3acc527482a9aa32a837898a4e063c2c93c

--- nemu/src/cpu/exec/control-transfer/leave.c
+++ nemu/src/cpu/exec/control-transfer/leave.c
@@ -1,4 +1,5 @@
 #include "cpu/exec/helper.h"
+
 #define instr leave
 
 
@@ -6,6 +7,8 @@ int  leave(swaddr_t eip) {
 	cpu.esp=cpu.ebp;
 	cpu.ebp=swaddr_read(cpu.esp,4);
 	cpu.esp+=4;
+	
+	print_asm_template1();
 	return 1;
 }
 
