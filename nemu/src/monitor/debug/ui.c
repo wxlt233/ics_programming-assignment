5fffc14a6dffe6ff4a4d2cb404f90ebf5e627f2b
tracer-ics2015 <tracer@njuics.org>
2015-11-24 20:14:41 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:14:41 up  2:37,  4 users,  load average: 0.01, 0.03, 0.05 cdcba47d2a371f30107bf011e5e1d3f86b86ebb9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -88,7 +88,7 @@ static int cmd_x(char *args)
    s=arg2;
    bool pan=true;
    printf("%s:       ",arg2);
-   add=expr("aa",&pan);
+   add=expr(arg2,&pan);
    if (!pan) 
    {
 	   printf("wrong input!\n");return 0;
