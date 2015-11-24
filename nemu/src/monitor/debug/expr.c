fa789a993567b4d1cd404aff03a94178909c319f
tracer-ics2015 <tracer@njuics.org>
2015-11-24 23:12:01 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:12:01 up 26 min,  3 users,  load average: 0.00, 0.02, 0.05 ca5feb728db945263221b5044dc786f7e4197c44

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -80,7 +80,7 @@ static bool make_token(char *e) {
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
 			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-				char *substr_start = e + position;
+		//		char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
 			//	Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
@@ -365,7 +365,7 @@ uint32_t expr(char *e, bool *success) {
      x=eval(0,nr_token-1);
 	 int ik;
 	 for (ik=0;ik<nr_token;ik++)
-	
+	 {	
 		 int jk;
 		 for (jk=0;jk<32;jk++)
 		 tokens[ik].str[jk]='\0';
