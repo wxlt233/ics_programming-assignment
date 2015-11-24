edfbc014193f132816d5e2d84507b4a43534d3e8
tracer-ics2015 <tracer@njuics.org>
2015-11-24 21:21:32 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:21:32 up 18 min,  3 users,  load average: 0.00, 0.04, 0.05 1255e0e3a5a902fe29b207886665927bc3d23039

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -38,7 +38,7 @@ static struct rule {
 	{"\\$\\w{2,3}",REG},                  //register
 	{"*",DEREF},                       //pointer
 	{"-",NEG},               //NEG
-	{"aa",VAR}              //variable
+	{"zz",VAR}              //variable
 
 };
 
