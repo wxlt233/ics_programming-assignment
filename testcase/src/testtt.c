87f902b2663f22a9b4d476dbc64525b236844183
tracer-ics2015 <tracer@njuics.org>
2015-11-25 22:53:25 +0800

    >  run 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:53:25 up 4 min,  3 users,  load average: 0.06, 0.28, 0.16 9d8f5a928b4b278678d9da0c600d86ffdbe54d3

---
+++ testcase/src/testtt.c
@@ -0,0 +1,13 @@
+#include <trap.h>
+int func(int n)
+{
+	if (n<=2) return 1;
+	else return (func(n-1)+func(n-2));
+}
+int main()
+{
+	int a=func(5);
+	nemu_assert(a== 5);
+	HIT_GOOD_TRAP;
+	return 0;	
+}
