10a6640d54fbf1beab3d05fc089fe8c7183c0032
tracer-ics2015 <tracer@njuics.org>
2015-11-25 12:52:39 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:52:39 up  2:58,  7 users,  load average: 0.08, 0.03, 0.05 174a7697d7a1a9ebad1de8b5cb369238f16892ad

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -98,21 +98,21 @@ void load_elf_tables(int argc, char *argv[]) {
 			assert(ret == 1);
 		}
 	}
-//	printf("%x\n",(unsigned)strtab);
+/*printf("%x\n",(unsigned)strtab);
 	int ii;
 	for (ii=0;ii<nr_symtab_entry;ii++)
 	{
 		int jj=0;
-		printf("%x\n",symtab[ii].st_name);
+		//printf("%x\n",symtab[ii].st_name);
 		while (*(jj+strtab+symtab[ii].st_name))
 		{
 			printf("%c",*(jj+strtab+symtab[ii].st_name));
 		    jj++;	
 		}
-//		printf("info: %c",symtab[ii].st_info);
+		printf("info: %c",symtab[ii].st_info);
 		printf("\n");
 	}
-
+*/
 	free(sh);
 	free(shstrtab);
 
