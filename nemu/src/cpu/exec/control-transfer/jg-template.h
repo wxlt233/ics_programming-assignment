de29a4eaa4aadb8909133d3d571a6a40099428b9
tracer-ics2015 <tracer@njuics.org>
2015-11-26 19:10:38 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:10:38 up  3:43,  4 users,  load average: 0.08, 0.03, 0.05 d1792fadc725807d4d38126160b9a1ca90ec2bb

--- nemu/src/cpu/exec/control-transfer/jg-template.h
+++ nemu/src/cpu/exec/control-transfer/jg-template.h
@@ -7,7 +7,7 @@ static void do_execute() {
  {
  	 char t=op_src->val&0x000000ff;
 //	 printf("%d",t);
-	if (cpu.eflags.ZF==0&&cpu.eflags.CF==cpu.eflags.SF) cpu.eip+=t;
+	if (cpu.eflags.ZF==0&&cpu.eflags.OF==cpu.eflags.SF) cpu.eip+=t;
  }
 	//	(int  )(op_src->val&0xff);
 	print_asm_template1();
