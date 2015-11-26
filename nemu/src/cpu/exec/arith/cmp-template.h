9a52c609bfd7be169cf74d1c349bd3d9330325d5
tracer-ics2015 <tracer@njuics.org>
2015-11-26 19:18:45 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:18:45 up  3:51,  4 users,  load average: 0.02, 0.02, 0.05 d9eb6b1ee2db6863b4608c6bfb0670acf55ccdf1

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -9,7 +9,7 @@ static void do_execute() {
 	//if (b>0&&a-b>a) cpu.eflags.OF=1;
 	//else if (b<0&&a-b<a) cpu.eflags.OF=1;
 	if (MSB(a)==0&&MSB(b)==1&&MSB(result)==1 ) cpu.eflags.OF=1;
-	else if (MSB(a)==1&&MSB(b)==0&&MSB(result==0)) cpu.eflags.OF=1;
+	else if (MSB(a)==1&&MSB(b)==0&&MSB(result)==0) cpu.eflags.OF=1;
 	else cpu.eflags.OF=0;
 	if (result==0)
 	   	cpu.eflags.ZF=1;
--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -7,7 +7,7 @@ static void do_execute() {
     DATA_TYPE a=op_dest->val;
 	DATA_TYPE b=op_src->val;	
 	if (MSB(a)==0&&MSB(b)==1&&MSB(result)==1 ) cpu.eflags.OF=1;
-	else if (MSB(a)==1&&MSB(b)==0&&MSB(result==0)) cpu.eflags.OF=1;
+	else if (MSB(a)==1&&MSB(b)==0&&MSB(result)==0) cpu.eflags.OF=1;
 	else cpu.eflags.OF=0;
 	if (result==0)
 	   	cpu.eflags.ZF=1;
