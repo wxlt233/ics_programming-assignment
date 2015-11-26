9df281dda2afb9e0b94ca3495b0316a6618b6e55
14307130132-Li Tong <14307130132@fudan.edu.cn>
2015-11-26 17:01:57 +0800

    fix some bug with watchpoint

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -38,7 +38,7 @@ void do_int3() {
 /* Simulate how the CPU works. */
 void cpu_exec(volatile uint32_t n) {
 
-	int t=(int) n;
+	int t=(int) n;          //when  cpu_exec(-1) 
 	if(nemu_state == END) {
 		printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
 		return;
