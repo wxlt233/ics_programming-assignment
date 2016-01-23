0535b60b0124aaba4a937824b081448e13ffd261
tracer-ics2015 <tracer@njuics.org>
2016-01-24 00:54:51 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:54:51 up  8:37,  8 users,  load average: 0.00, 0.01, 0.05 9368e04443347027564887697eee12f8ee024fa7

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -3,16 +3,16 @@
 #define instr movsx
 
 static void do_execute() {
-	if (DATA_BYTE==2)
+/*	if (DATA_BYTE==2)
 	{
 		short t=op_src->val;
 		OPERAND_W(op_dest,t);
-	}
-	if (DATA_BYTE==4)
-	{
+	}*/
+	//if (DATA_BYTE==4)
+//	{
 	int t=op_src->val;
 	OPERAND_W(op_dest,t);
-	}
+//	}
 	print_asm_template2();
 }
 
