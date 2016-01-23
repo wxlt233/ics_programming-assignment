46e49bc5c232380e251ba57453649812f8f5bdfd
tracer-ics2015 <tracer@njuics.org>
2016-01-23 19:28:07 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:28:07 up  3:10,  4 users,  load average: 0.09, 0.04, 0.05 e7ea7d342f7f15c98ff82f41bb1642e73c05451

--- nemu/src/cpu/exec/logic/or-template.h
+++ nemu/src/cpu/exec/logic/or-template.h
@@ -5,10 +5,20 @@
 static void do_execute () {
 	DATA_TYPE result = op_dest->val | op_src->val;
 	OPERAND_W(op_dest, result);
-
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	cpu.eflags.OF=0;
+	if (result==0)
+	   	cpu.eflags.ZF=1;
+	else 
+		cpu.eflags.ZF=0;
+	cpu.eflags.SF=MSB(result);
+	cpu.eflags.CF=0;
+	short t=(result&1)+((result>>1)&1)+((result>>2)&1)
+		+((result>>3)&1)+((result>>4)&1)+((result>>5)&1)
+		+((result>>6)&1)+((result>>7)&1);
+	if (t%2==0) 
+		cpu.eflags.PF=1;
+	else 
+		cpu.eflags.PF=0;
 	print_asm_template2();
 }
 
