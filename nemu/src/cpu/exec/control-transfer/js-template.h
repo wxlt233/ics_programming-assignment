254f0868282b74ff8493b4961790802d571f0a74
tracer-ics2015 <tracer@njuics.org>
2016-01-24 12:45:53 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:45:53 up 20:28,  4 users,  load average: 0.09, 0.15, 0.13 ac39ee023dfc26c618e21b18920e0a0ee2846a2

--- nemu/src/cpu/exec/control-transfer/js-template.h
+++ nemu/src/cpu/exec/control-transfer/js-template.h
@@ -8,7 +8,7 @@ static void do_execute() {
  	 char t=op_src->val&0x000000ff;
 	if (cpu.eflags.SF==1) cpu.eip+=t;
  }
- else if (DATA_BYTE)
+ else if (DATA_BYTE==2)
  {
 	short t=op_src->val&0x0000ffff;
 	if (cpu.eflags.SF==1)  cpu.eip+=t;
