8c3c5ae9973d36cbe21be3898f4018814a49cb31
tracer-ics2015 <tracer@njuics.org>
2016-01-28 00:38:52 +0800

    >  run 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:38:52 up 16:45,  7 users,  load average: 0.00, 0.01, 0.05 f045fd52a775f9a9a1735d902ae41d7d700f2fce

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -17,10 +17,7 @@ FLOAT f2F(float a) {
 	int exp=((aint>>23)&0xff)-127;
 	int sign=(aint>>31)&1;
 	int f2=(aint&0x7fffff)|0x800000;
-	int offset=23-exp;
-	int t=((f2>>offset)<<16)&0x7fff0000;
-	f2=((f2<<(32-offset))>>16)&0x0000ffff;
-	int F=f2+t;
+	int F=((f2<<exp)<<16);
 	if (sign==1) return -F;
 	else return F;
 
