b20dfbc03b6a199b21129fa2fbf249d5d4914a2f
tracer-ics2015 <tracer@njuics.org>
2016-01-27 20:26:55 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:26:55 up 12:33,  3 users,  load average: 0.03, 0.04, 0.05 2378f781181c9f33c5e0dd1decd80013cc58f84

--- nemu/src/cpu/exec/control-transfer/jmp-template.h
+++ nemu/src/cpu/exec/control-transfer/jmp-template.h
@@ -3,6 +3,8 @@
 #define instr jmp
 
 static void do_execute() {
+	if (op_src->type==2)
+	{
  if (DATA_BYTE==1)
  {
  	 char t=op_src->val&0x000000ff;
@@ -13,6 +15,8 @@ static void do_execute() {
 	 short t=op_src->val&0x0000ffff;
 	 cpu.eip=(cpu.eip+t)&0x0000ffff;
  }
+	}
+	else printf("haha");
 	print_asm_template1();
 }
 
