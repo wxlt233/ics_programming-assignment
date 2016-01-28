dc8ce9e6b807beb0bb8f981d044bf9859452c9f3
tracer-ics2015 <tracer@njuics.org>
2016-01-28 23:14:35 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:14:35 up  1:58,  4 users,  load average: 0.00, 0.01, 0.05 aacff9868148a646c8aab4e34f42711ebade5ede

--- nemu/src/cpu/exec/control-transfer/call.c
+++ nemu/src/cpu/exec/control-transfer/call.c
@@ -3,16 +3,18 @@
 /*#define DATA_BYTE 1
 #include "call-template.h"
 #undef DATA_BYTE
-
+*/
 #define DATA_BYTE 2
 #include "call-template.h"
 #undef DATA_BYTE
-*/
+
 
 #define DATA_BYTE 4
 #include "call-template.h"
 #undef DATA_BYTE
 
+make_helper_v(call_i)
+make_helper_v(call_rm)
 /* for instruction encoding overloading */
 
 
--- nemu/src/cpu/exec/control-transfer/call.h
+++ nemu/src/cpu/exec/control-transfer/call.h
@@ -1,8 +1,8 @@
 #ifndef __CALL_H__
 #define __CALL_H__
 
-make_helper(call_i_l);
-make_helper(call_rm_l);
+make_helper(call_i_v);
+make_helper(call_rm_v);
 #endif
 
 
