2d398b999ae0d24ecacdff4363216cab9216c7f1
tracer-ics2015 <tracer@njuics.org>
2016-01-28 22:15:08 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:15:08 up 59 min,  3 users,  load average: 0.00, 0.01, 0.05 d31de5ca1c91a74a6bf437cc2adefb425b74ebb1

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -5,14 +5,18 @@ static void do_execute() {
 	DATA_TYPE result=op_dest->val+op_src->val+cpu.eflags.CF;
     DATA_TYPE a=op_dest->val;
 	DATA_TYPE b=op_src->val;	
+
 	if (MSB(a)==1&&MSB(b)==1&&MSB(result)==0 ) cpu.eflags.OF=1;
 	else if (MSB(a)==0&&MSB(b)==0&&MSB(result)==1) cpu.eflags.OF=1;
 	else cpu.eflags.OF=0;
+
 	if (result==0)
 	   	cpu.eflags.ZF=1;
 	else 
 		cpu.eflags.ZF=0;
+
 	cpu.eflags.SF=MSB(result);
+
 	if (a+b<a||a+b<b) 
 		cpu.eflags.CF=1;
 	else 
