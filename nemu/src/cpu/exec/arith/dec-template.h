bb29f74b72ba6960150ec0380647c256480ba9d2
tracer-ics2015 <tracer@njuics.org>
2016-01-28 22:17:08 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:17:08 up  1:01,  3 users,  load average: 0.13, 0.07, 0.06 52e9f4287a57b9fae3d5d150923c3e3be5ea4d99

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -5,9 +5,25 @@
 static void do_execute () {
 	DATA_TYPE result = op_src->val - 1;
 	OPERAND_W(op_src, result);
+    
+	DATA_TYPE a=op_dest->val;
+	DATA_TYPE b=1;	
+	if (MSB(a)==0&&MSB(b)==1&&MSB(result)==1 ) cpu.eflags.OF=1;
+	else if (MSB(a)==1&&MSB(b)==0&&MSB(result)==0) cpu.eflags.OF=1;
+	else cpu.eflags.OF=0;
+	if (result==0)
+	   	cpu.eflags.ZF=1;
+	else 
+		cpu.eflags.ZF=0;
+	cpu.eflags.SF=MSB(result);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	short t=(result&1)+((result>>1)&1)+((result>>2)&1)
+		+((result>>3)&1)+((result>>4)&1)+((result>>5)&1)
+		+((result>>6)&1)+((result>>7)&1);
+	if (t%2==0) 
+		cpu.eflags.PF=1;
+	else 
+		cpu.eflags.PF=0;
 
 	print_asm_template1();
 }
