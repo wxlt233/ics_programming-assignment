3cfe1d835a98243f5746800bfd327aee6cda8ba3
tracer-ics2015 <tracer@njuics.org>
2016-01-29 10:28:29 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:28:29 up  2:08,  3 users,  load average: 0.11, 0.06, 0.05 d275f7da444c925877ed2a89ba7ec23e53c3279

--- nemu/src/cpu/exec/control-transfer/ret-template.h
+++ nemu/src/cpu/exec/control-transfer/ret-template.h
@@ -18,11 +18,11 @@ make_helper(concat(ret_,SUFFIX))
 make_helper(concat(ret_i_,SUFFIX))
 {
 	swaddr_t addr=instr_fetch(cpu.eip+1,DATA_BYTE);
-	cpu.eip=swaddr_read(cpu.esp,4)-3;
+	cpu.eip=swaddr_read(cpu.esp,4);
 	cpu.esp+=cpu.esp+addr;
 	if (DATA_BYTE==2) cpu.eip=cpu.eip&0x0000ffff;
 	print_asm_template1();
-	return 3;
+	return 0;
 }
 
 #include "cpu/exec/template-end.h"
