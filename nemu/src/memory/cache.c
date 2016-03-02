c183fdaf0d26b8ba0c0f50757df1a1aeb5386e3a
tracer-ics2015 <tracer@njuics.org>
2016-03-02 23:17:44 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:17:44 up 13:32,  6 users,  load average: 0.18, 0.25, 0.22 3947a56c2fb1b017d2860c6434e40d37c8907631

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -232,8 +232,8 @@ void write_cache1_hit(hwaddr_t addr,size_t len ,uint32_t data)
 	else 
 	{
 		//printf("error!");
-	//	write_allocate(addr,len,datat);
-		dram_write(addr,len,datat);
+		write_allocate(addr,len,datat);
+	//	dram_write(addr,len,datat);
 	}
 }
 
