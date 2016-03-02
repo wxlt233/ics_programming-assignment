3cb793dd861fe421fec0c1402fff69ca63b76eef
tracer-ics2015 <tracer@njuics.org>
2016-03-03 00:50:29 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:50:29 up 15:04,  4 users,  load average: 0.09, 0.03, 0.05 6ce098cf65eb8e985834c95d7ac93c55b550b195

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -2,28 +2,49 @@
 #include<time.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 #define BlockSize 64
+#define bitofblock 6
 
+#define maskofblock 0x3f
+#define maskofzushu1 0x7f
+#define maskofzushu2 0xfff
+
+#define cache1size 65536
+#define cache2size (4*1024*1024)
+
+#define cache1way 8
+#define cache2way 16
+
+#define Zushu1 (cache1size/BlockSize/cache1way)  //128
+#define Zushu2 (cache2size/BlockSize/cache2way)  //4096
+
+#define bitofzushu1 ((int)(round(log(Zushu1)/log(2))))
+#define bitofzushu2 ((int) (round(log(Zushu2)/log(2))))
+ 
+#define gettag1 (bitofblock+bitofzushu1)
+#define gettag2 (bitofblock+bitofzushu2)
 
 
 typedef struct 
 {
-	uint8_t offset[64];
+	uint8_t offset[BlockSize];
 	uint8_t valid;
 	uint32_t tag;
 }cache1block;
 
-cache1block cache1[128][8];
+cache1block cache1[Zushu1][cache1way];
 
 typedef struct 
 {
-	uint8_t offset[64];
+	uint8_t offset[BlockSize];
 	uint8_t valid;
 	uint8_t dirty;
 	uint32_t tag;
 }cache2block;
-cache2block cache2[4096][16];
+
+cache2block cache2[Zushu2][cache2way];
 
 uint32_t dram_read(hwaddr_t addr,size_t len);
 uint32_t dram_write(hwaddr_t addr,size_t len,uint32_t data);
@@ -34,12 +55,12 @@ void write_allocate(hwaddr_t addr,size_t len,uint32_t data);
 void init_cache()
 {
 	int i,j;
-	for (i=0;i<128;i++)
-	  for (j=0;j<8;j++)
+	for (i=0;i<Zushu1;i++)
+	  for (j=0;j<cache1way;j++)
 		  cache1[i][j].valid=0;
-	for (i=0;i<4096;i++)
-	  for (j=0;j<16;j++)
-	  {  cache2[i][j].valid=0;
+	for (i=0;i<Zushu2;i++)
+	  for (j=0;j<cache2way;j++)
+ 	  {  cache2[i][j].valid=0;
 		  cache2[i][j].dirty=0;
 	  }
 }
@@ -48,10 +69,10 @@ bool checkcache1(hwaddr_t addr)
 {
   uint32_t tag1;
   uint32_t cacheno;
-  tag1=addr>>13;
-  cacheno=(addr>>6)&0x7f;
+  tag1=addr>>gettag1;
+  cacheno=(addr>>bitofblock)&maskofzushu1;
   int i;
-  for (i=0;i<8;i++)
+  for (i=0;i<cache1way;i++)
 	 if (cache1[cacheno][i].tag==tag1&&cache1[cacheno][i].valid)
 		 return 1;
   return 0;
@@ -61,10 +82,10 @@ bool checkcache2(hwaddr_t addr)
 {
 	uint32_t tag2;
 	uint32_t cacheno;
-	tag2=addr>>18;
-	cacheno=(addr>>6)&0xfff;
+	tag2=addr>>gettag2;
+	cacheno=(addr>>bitofblock)&maskofzushu2;
 	int i;
-	for (i=0;i<16;i++)
+	for (i=0;i<cache2way;i++)
 		if (cache2[cacheno][i].tag==tag2&&cache2[cacheno][i].valid)
 			return 1;
 	return 0;
@@ -74,18 +95,18 @@ bool checkcache2(hwaddr_t addr)
 void read_cache2tocache1(hwaddr_t addr)
 {
 	uint32_t tag1,tag2,cache1no,cache2no;
-	tag1=addr>>13;
-	tag2=addr>>18;
-	cache1no=(addr>>6)&0x7f;
-	cache2no=(addr>>6)&0xfff;
+	tag1=addr>>gettag1;
+	tag2=addr>>gettag2;
+	cache1no=(addr>>bitofblock)&maskofzushu1;
+	cache2no=(addr>>bitofblock)&maskofzushu2;
 	int i;
-	for (i=0;i<16;i++)
+	for (i=0;i<cache2way;i++)
 		if (cache2[cache2no][i].valid==1&&cache2[cache2no][i].tag==tag2)
 			break;
 	int j;
 	srand(time(0)+clock());
-	int ran=rand()%8;
-	for (j=0;j<64;j++)
+	int ran=rand()%cache1way;
+	for (j=0;j<BlockSize;j++)
 		cache1[cache1no][ran].offset[j]=cache2[cache2no][i].offset[j];
 	cache1[cache1no][ran].valid=1;
 	cache1[cache1no][ran].tag=tag1;
@@ -93,39 +114,37 @@ void read_cache2tocache1(hwaddr_t addr)
 
 void read_dramtocache2(hwaddr_t addr)
 {
-	uint32_t tag2=addr>>18;
-	uint32_t cache2no=(addr>>6)&0xfff;
+	uint32_t tag2=addr>>(gettag2);
+	uint32_t cache2no=(addr>>bitofblock)&maskofzushu2;
 	srand(time(0)+clock());
-	int ran=rand()%16;
+	int ran=rand()%cache2way;
 	int i;
-//	printf("random dramtocache2 %d\n",ran);
 	if (cache2[cache2no][ran].valid&&cache2[cache2no][ran].dirty)
-	{
-		uint32_t writebackaddr=(cache2[cache2no][ran].tag<<18)+(cache2no<<6);
-		for (i=0;i<64;i++)
+ 	{
+		uint32_t writebackaddr=(cache2[cache2no][ran].tag<<gettag2)+(cache2no<<bitofblock);
+		for (i=0;i<BlockSize;i++)
 			dram_write(writebackaddr+i,1,cache2[cache2no][ran].offset[i]);
 	}
-	uint32_t beginaddr=(addr>>6)<<6;
+	uint32_t beginaddr=(addr>>bitofblock)<<bitofblock;
 	cache2[cache2no][ran].dirty=0;
 	cache2[cache2no][ran].valid=1;
 	cache2[cache2no][ran].tag=tag2;
-	for (i=0;i<64;i++)
+	for (i=0;i<BlockSize;i++)
 		cache2[cache2no][ran].offset[i]=dram_read(beginaddr+i,1);
 
 }
 uint32_t read_cache1_hit(hwaddr_t addr,size_t len)
 {
-	uint32_t tag1=addr>>13;
-	uint32_t cache1no=(addr>>6)&0x7f;
+	uint32_t tag1=addr>>gettag1;
+	uint32_t cache1no=(addr>>bitofblock)&maskofzushu1;
 	int i;
-	for (i=0;i<8;i++)
-	{
+	for (i=0;i<cache1way;i++)
+ 	{
 		if (cache1[cache1no][i].valid&&cache1[cache1no][i].tag==tag1) break;
 	}
-//	printf("cache1hit %d\n",i);
-	uint32_t offset=addr&0x3f;
+	uint32_t offset=addr&maskofblock;
 	uint32_t data=0;
-	if (offset+len-1<64)
+	if (offset+len-1<BlockSize)
 	{
 		int j;
 		for (j=0;j<len;j++)
@@ -135,31 +154,28 @@ uint32_t read_cache1_hit(hwaddr_t addr,size_t len)
 	else 
 	{
 		int j;
-		for (j=offset;j<64;j++)
+		for (j=offset;j<BlockSize;j++)
 			data+=(cache1[cache1no][i].offset[j])<<(8*(j-offset));
-	//	printf("%d %d\n",offset,len);
-//		printf("%x\n",addr);
-		uint32_t nextblockaddr=((addr+64)>>6)<<6;
-	//	printf("%x %d\n",nextblockaddr,len-(64-offset));
-		uint32_t t=hwaddr_read(nextblockaddr,len-(64-offset));
-		data+=t<<(8*(64-offset));
+		uint32_t nextblockaddr=((addr+BlockSize)>>bitofblock)<<bitofblock;
+		uint32_t t=hwaddr_read(nextblockaddr,len-(BlockSize-offset));
+		data+=t<<(8*(BlockSize-offset));
 		return data;
 	}
 }
 
 uint32_t read_cache2_hit(hwaddr_t addr,size_t len)
 {
-	uint32_t tag2=addr>>18;
-	uint32_t cache2no=(addr>>6)&0xfff;
+	uint32_t tag2=addr>>gettag2;
+	uint32_t cache2no=(addr>>bitofblock)&maskofzushu2;
 	int i;
-	for (i=0;i<16;i++)
+	for (i=0;i<cache2way;i++)
 	{
 		if (cache2[cache2no][i].valid&&cache2[cache2no][i].tag==tag2)
 			break;
 	}
-	uint32_t offset=addr&0x3f;
+	uint32_t offset=addr&maskofblock;
 	uint32_t data=0;
-	if (offset+len-1<64)
+	if (offset+len-1<BlockSize)
 	{
 		int j;
 		for (j=0;j<len;j++)
@@ -169,90 +185,88 @@ uint32_t read_cache2_hit(hwaddr_t addr,size_t len)
 	else 
 	{
 		int j;
-		for (j=offset;j<64;j++)
+		for (j=offset;j<BlockSize;j++)
 			data+=(cache2[cache2no][i].offset[j]<<(8*(j-offset)));
-		uint32_t nextblockaddr=((addr+64)>>6)<<6;
-		uint32_t t=hwaddr_read(nextblockaddr,len-(64-offset));
-		data+=t<<(8*(64-offset));
+		uint32_t nextblockaddr=((addr+BlockSize)>>bitofblock)<<bitofblock;
+		uint32_t t=hwaddr_read(nextblockaddr,len-(BlockSize-offset));
+		data+=t<<(8*(BlockSize-offset));
 		return data;
 	}
 }
 
 void write_cache1_hit(hwaddr_t addr,size_t len ,uint32_t data)
 {  //write through
-	uint32_t tag1=addr>>13;
-	uint32_t cache1no=(addr>>6)&0x7f;
+	uint32_t tag1=addr>>gettag1;
+	uint32_t cache1no=(addr>>bitofblock)&maskofzushu1;
 	int i;
 	uint32_t datat=data;
 	int cache1zunei=0;
-	for (i=0;i<8;i++)
+	for (i=0;i<cache1way;i++)
 		if (cache1[cache1no][i].valid&&cache1[cache1no][i].tag==tag1)
 		{
 			cache1zunei=i;
 			break;
 		}
-	uint32_t offset=addr&0x3f;
+	uint32_t offset=addr&maskofblock;
 	int j;
 	//write cache1
-	if (offset+len-1<64)
+	if (offset+len-1<BlockSize)
 	{
 		for (j=0;j<len;j++)
 			cache1[cache1no][cache1zunei].offset[offset+j]=(data>>(8*j))&0xff;
 	}
 	else 
 	{
-		for (j=offset;j<64;j++)
+		for (j=offset;j<BlockSize;j++)
 		{
 			cache1[cache1no][cache1zunei].offset[j]=data&0xff;
 			data=data>>8;
 		}
-		hwaddr_write(((addr+64)>>6)<<6,len-(64-offset),data);
+		hwaddr_write(((addr+BlockSize)>>bitofblock)<<bitofblock,len-(BlockSize-offset),data);
 	}
 	//write cache2 write through
-	uint32_t tag2=addr>>18;
-	uint32_t cache2no=(addr>>6)&0xfff;
-	uint32_t cache2zunei=16;
-	for (i=0;i<16;i++)
+	uint32_t tag2=addr>>gettag2;
+	uint32_t cache2no=(addr>>bitofblock)&maskofzushu2;
+	uint32_t cache2zunei=cache2way;
+	for (i=0;i<cache2way;i++)
 		if (cache2[cache2no][i].valid&&cache2[cache2no][i].tag==tag2)
-		{
+		{ 
 			cache2zunei=i;
 		   	break;
  		}
-	if (cache2zunei<16)
+	if (cache2zunei<cache2way)
 	{
 		cache2[cache2no][cache2zunei].dirty=1;
-		if (offset+len-1<64)
+		if (offset+len-1<BlockSize)
 		{
 			for (j=0;j<len;j++)
 			cache2[cache2no][cache2zunei].offset[offset+j]=cache1[cache1no][cache1zunei].offset[offset+j];
 		}
 		else 
 		{
-			for (j=offset;j<64;j++)
+			for (j=offset;j<BlockSize;j++)
 			cache2[cache2no][cache2zunei].offset[j]=cache1[cache1no][cache1zunei].offset[j];
-		//	hwaddr_write(((addr+64)>>6)<<6,len-(64-offset),data);
 		}
 	}
 	else 
 	{
 		write_allocate(addr,len,datat);
-	//	dram_write(addr,len,datat);
 	}
 }
 
 
 void write_cache2_hit(hwaddr_t addr,size_t len,uint32_t data)
 {
-	uint32_t tag2=addr>>18;
-	uint32_t cache2no=(addr>>6)&0xfff;
+	uint32_t tag2=addr>>gettag2;
+	uint32_t cache2no=(addr>>bitofblock)&maskofzushu2;
 	int i;
-	for (i=0;i<16;i++)
+	for (i=0;i<cache2way;i++)
 		if (cache2[cache2no][i].valid&&cache2[cache2no][i].tag==tag2)
 			break;
 	cache2[cache2no][i].dirty=1;
-	uint32_t offset=addr&0x3f;
+	uint32_t offset=addr&maskofblock;
 	int j;
-	if (offset+len-1<64)
+	if (offset+len-1<BlockSize)
 	{
 		for (j=0;j<len;j++)
 		{
@@ -262,35 +276,35 @@ void write_cache2_hit(hwaddr_t addr,size_t len,uint32_t data)
 	}
 	else 
 	{
-		for (j=offset;j<64;j++)
+		for (j=offset;j<BlockSize;j++)
 		{
 			cache2[cache2no][i].offset[j]=data&0xff;
 			data=data>>8;
 		}
-		hwaddr_write(((addr+64)>>6)<<6,len-(64-offset),data);
+		hwaddr_write(((addr+BlockSize)>>bitofblock)<<bitofblock,len-(BlockSize-offset),data);
 	}
 }
 
 void write_allocate(hwaddr_t addr,size_t len,uint32_t data)
 {
-	uint32_t tag2=addr>>18;
-	uint32_t cache2no=(addr>>6)&0xfff;
+	uint32_t tag2=addr>>gettag2;
+	uint32_t cache2no=(addr>>bitofblock)&maskofzushu2;
 	dram_write(addr,len,data);
 	srand(time(0)+clock());
-	int i=rand()%16;
+	int i=rand()%cache2way;
 	if (cache2[cache2no][i].valid&&cache2[cache2no][i].dirty)
 	{
 		int j;
-		uint32_t writebackaddr=(cache2[cache2no][i].tag<<18)+(cache2no<<6);
-		for (j=0;j<64;j++)
+		uint32_t writebackaddr=(cache2[cache2no][i].tag<<gettag2)+(cache2no<<bitofblock);
+		for (j=0;j<BlockSize;j++)
 			dram_write(writebackaddr+j,1,cache2[cache2no][i].offset[j]);
 	}
 	cache2[cache2no][i].valid=1;
 	cache2[cache2no][i].dirty=0;
 	cache2[cache2no][i].tag=tag2;
 	int j;
-	uint32_t readbeginaddr=(addr>>6)<<6;
-	for (j=0;j<64;j++)
+	uint32_t readbeginaddr=(addr>>bitofblock)<<bitofblock;
+	for (j=0;j<BlockSize;j++)
 		cache2[cache2no][i].offset[j]=dram_read(readbeginaddr+j,1);
 }
 
