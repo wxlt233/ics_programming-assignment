b55ba50cf27f80ceef7aff6df814d089e1cfdb5b
tracer-ics2015 <tracer@njuics.org>
2016-03-11 12:12:59 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:12:59 up  2:14,  3 users,  load average: 0.01, 0.04, 0.06 ead9a0c2f38738e1cf404e9996f60cc21486b98

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -19,7 +19,7 @@ void write_allocate(hwaddr_t addr,size_t len,uint32_t data);
 /*uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }*/
-
+int bss=0;
 uint32_t hwaddr_read(hwaddr_t addr,size_t len)
 {
 	if (checkcache1(addr))
@@ -102,7 +102,6 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data,uint8_t sreg) {
 #endif
 	lnaddr_t lnaddr=seg_translate(addr,len,sreg);
 	lnaddr_write(lnaddr, len, data);
-	int bss=0;
 	if (bss>1) printf("hahah");
 }
 
