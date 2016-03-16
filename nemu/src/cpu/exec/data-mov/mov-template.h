0a183bb02caca73fdf371597ef381c2b69a47b6c
tracer-ics2015 <tracer@njuics.org>
2016-03-16 11:05:04 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:05:04 up  1:40,  3 users,  load average: 0.02, 0.05, 0.05 38259f513f06889938b3a386dc72dc2ab670df1f

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -79,11 +79,11 @@ make_helper(concat(mov_r2sr_,SUFFIX))
 	cpu.DESC[sreg].limit_19_16=lnaddr_read(descaddr+6,1)&0xf;
 	cpu.DESC[sreg].base_31_24=lnaddr_read(descaddr+7,1)&0xff;
 	if (sreg==0)
-		print_asm("%%%s %%es",REG_NAME(op_src->reg));
+		print_asm("mov %%%s %%es",REG_NAME(op_src->reg));
 	else if (sreg==2)
-		print_asm("%%%s %%ds",REG_NAME(op_src->reg));
+		print_asm("mov %%%s %%ds",REG_NAME(op_src->reg));
 	else 
-		print_asm("%%%s %%ss",REG_NAME(op_src->reg));
+		print_asm("mov %%%s %%ss",REG_NAME(op_src->reg));
 	return len+1;
 }
 
