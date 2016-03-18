ebebf9ca8979101907d54d57e5ec743883114fee
tracer-ics2015 <tracer@njuics.org>
2016-03-18 17:27:31 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:27:31 up  6:10,  2 users,  load average: 0.16, 0.06, 0.06 4c41ba9f709ed1d1473ec3755a01d8a3e50f8f31

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -58,6 +58,7 @@ hwaddr_t  page_translate(lnaddr_t addr)
 {
 	if (cpu.cr0.protect_enable==1&&cpu.cr0.paging==1)
 	{
+		printf("ok");
 		uint16_t offset=addr&0xfff;
 		uint16_t dir=addr>>22;
 		uint16_t page=(addr>>12)&0x3fff;
