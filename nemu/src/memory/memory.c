3d1949f8aecd24ac90a62ab72d9b54af793f82ae
tracer-ics2015 <tracer@njuics.org>
2016-03-19 09:57:23 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  09:57:23 up  1:00,  4 users,  load average: 0.05, 0.03, 0.05 c56252f16ec4d7c16d1a2ddf83495e7ce8c824

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -61,8 +61,14 @@ hwaddr_t  page_translate(lnaddr_t addr)
 		uint16_t offset=addr&0xfff;
 		uint16_t dir=addr>>22;
 		uint16_t page=(addr>>12)&0x3fff;
+		PDE aa;
+		aa.val=hwaddr_read((cpu.cr3.page_directory_base<<12)+4*dir,4);
+		assert(aa.present);
 		uint32_t pagetableaddr=hwaddr_read(((cpu.cr3.page_directory_base<<12)+4*dir),4)>>12;
 		uint32_t pagestartaddr=(hwaddr_read(((pagetableaddr<<12)+4*page),4)>>12)<<12;
+		PTE	a; 
+		a.val=hwaddr_read((pagetableaddr<<12)+4*page,4);
+		assert(a.present);
 		printf("0x%x 0x%x  0x%x\n",cpu.eip,addr,pagestartaddr+offset);
 		return pagestartaddr+offset;
 	}
