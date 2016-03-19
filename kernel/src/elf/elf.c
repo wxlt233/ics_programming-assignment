4e3ac8b777ca01666a7a2810a88ae07cbf0a25be
tracer-ics2015 <tracer@njuics.org>
2016-03-19 14:44:01 +0800

    >  compile kernel 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:44:01 up 10 min,  3 users,  load average: 0.06, 0.15, 0.10 3a73b09e628de7fe3a066238b2236fb647676b8a

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -64,8 +64,8 @@ uint32_t loader() {
 			uint32_t pa=mm_malloc(ph->p_vaddr,ph->p_memsz);
 #endif  
 
-			ramdisk_read((void*)pa,ELF_OFFSET_IN_DISK+ph->p_offset,ph->p_filesz); 
-			memset((void*)(pa+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
+			ramdisk_read((void*)pa+KOFFSET,ELF_OFFSET_IN_DISK+ph->p_offset,ph->p_filesz); 
+			memset((void*)(pa+ph->p_filesz+KOFFSET),0,ph->p_memsz-ph->p_filesz);
 		}
 	}
 
