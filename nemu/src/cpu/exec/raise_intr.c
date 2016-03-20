a783e0ea3d6916b7bc7337b7831747bcb6987128
tracer-ics2015 <tracer@njuics.org>
2016-03-20 19:47:26 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:47:26 up  3:10,  3 users,  load average: 0.08, 0.03, 0.05 6b31380b5839ea7141fa619ba4f73e852c4eaffc

--- nemu/src/cpu/exec/raise_intr.c
+++ nemu/src/cpu/exec/raise_intr.c
@@ -4,5 +4,14 @@ extern jmp_buf jbuf;
 
 void raise_intr(uint8_t NO)
 {
+	lnaddr_t addr=cpu.IDTR.base+8*NO;
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.eflags.val,2);
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.CS.val,2);
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.eip,2);
+	cpu.CS.val=lnaddr_read(addr+2,2);
+	cpu.eip=lnaddr_read(addr,2)+(lnaddr_read(addr,2)<<16);
 	longjmp(jbuf,1);
 }
