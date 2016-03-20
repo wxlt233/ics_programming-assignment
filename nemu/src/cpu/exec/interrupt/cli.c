006e2dce1977acffb8432c1a60d29f0d605946d0
14307130132-Li Tong <14307130132@fudan.edu.cn>
2016-03-20 20:39:33 +0800

    add pusha,popa,cli

---
+++ nemu/src/cpu/exec/interrupt/cli.c
@@ -0,0 +1,10 @@
+#include "nemu.h"
+#include "cpu/exec/helper.h"
+#include "cpu/decode/modrm.h"
+
+make_helper(cli)
+{
+	cpu.eflags.IF=0;
+}
+
+
---
+++ nemu/src/cpu/exec/interrupt/cli.h
@@ -0,0 +1,7 @@
+#ifndef __CLI_H__
+#define __CLI_H__
+
+make_helper(cli);
+
+#endif
+
