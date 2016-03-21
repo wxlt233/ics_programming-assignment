133c8b130ccf6427ff274d1ae0da9af96a017ae6
tracer-ics2015 <tracer@njuics.org>
2016-03-21 10:21:39 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:21:39 up  1:20,  4 users,  load average: 0.16, 0.05, 0.06 454dd5d4281afd94a797df8e6a91b5f315da9bf8

--- nemu/src/cpu/exec/lgdt/lidt-template.h
+++ nemu/src/cpu/exec/lgdt/lidt-template.h
@@ -5,7 +5,16 @@
 #if DATA_BYTE==2||DATA_BYTE==4
 make_helper(concat(lidt_i_,SUFFIX))
 {
+	int x=6;
 	uint32_t addr=instr_fetch(cpu.eip+3,4);
+	if (instr_fetch(eip+1,1)==0x10)
+	{
+		printf("hahah0x%x\n",cpu.eax);
+	//	addr=swaddr_read(cpu.eax,4,3);
+	//	addr=instr_fetch(addr,4,3);
+		x=2;
+		addr=cpu.eax;
+	}
 	if (DATA_BYTE==2)
  	{
 		uint16_t limit=swaddr_read(addr,2,3)&0xffff;
@@ -21,7 +30,7 @@ make_helper(concat(lidt_i_,SUFFIX))
 		cpu.IDTR.base=base;
 	}
 	print_asm("lidt $0x%x",addr);
-	return 6;
+	return x;
 }
 
 #endif
