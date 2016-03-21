bd2207ba562b63cbc0dd6e65d471d767503b9097
tracer-ics2015 <tracer@njuics.org>
2016-03-21 16:12:54 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:12:54 up  7:11,  6 users,  load average: 0.02, 0.05, 0.05 7674d86984bdaf5d63b9dbb4ae8477890e78484

--- nemu/src/cpu/exec/interrupt/iret.c
+++ nemu/src/cpu/exec/interrupt/iret.c
@@ -7,6 +7,12 @@ make_helper(iret)
 	cpu.eip=swaddr_read(cpu.esp,4,2);
 	cpu.esp+=4;
 	cpu.CS.val=swaddr_read(cpu.esp,4,2)&0xffff;
+	lnaddr_t descaddr=cpu.GDTR.base+8*cpu.CS.index;
+	cpu.DESC[1].limit_15_0=lnaddr_read(descaddr,2)&0xffff;
+	cpu.DESC[1].base_15_0=lnaddr_read(descaddr+2,2)&0xffff;
+	cpu.DESC[1].base_23_16=lnaddr_read(descaddr+4,1)&0xff;
+	cpu.DESC[1].limit_19_16=lnaddr_read(descaddr+6,1)&0xff;
+	cpu.DESC[1].base_31_24=lnaddr_read(descaddr+7,1)&0xff;
 	cpu.esp+=4;
 	cpu.eflags.val=swaddr_read(cpu.esp,4,2);
 	cpu.esp+=4;
