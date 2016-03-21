960e73c8b70e026cbac7401ffb61a7820d73e042
tracer-ics2015 <tracer@njuics.org>
2016-03-21 13:41:07 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:41:07 up  4:40,  7 users,  load average: 0.00, 0.01, 0.05 965aacc116979dd079119689eddea1a87ead8d54

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -27,7 +27,8 @@ void do_syscall(TrapFrame *tf) {
 
 		case SYS_brk: sys_brk(tf); break;
 		case SYS_write:{if (tf->ebx==1||tf->ebx==2)
-						   asm volatile(".byte 0xd6" ::"a"(2),"c"(tf->ecx),"d"(tf->edx));
+						    asm volatile(".byte 0xd6" ::"a"(2),"c"(tf->ecx),"d"(tf->edix));
+							tf->eax=tf->edx;
 					   }
 		/* TODO: Add more system calls. */
 
