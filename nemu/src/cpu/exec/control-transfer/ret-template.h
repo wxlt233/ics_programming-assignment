989e80b09667043f10393cbac7c0eb3a345b550f
tracer-ics2015 <tracer@njuics.org>
2016-03-24 18:33:12 +0800

    >  compile NEMU 14307130132 lt Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:33:12 up  3:30,  7 users,  load average: 0.00, 0.01, 0.05 d694cfdedf2fcbd1b0d8bcf7e566029c4864c116

--- nemu/src/cpu/exec/control-transfer/ret-template.h
+++ nemu/src/cpu/exec/control-transfer/ret-template.h
@@ -4,7 +4,7 @@
 
 make_helper(concat(ret_,SUFFIX))
 {
-	if (cpu.eip==0xc0100cb6) printf("hah\n");
+	if (cpu.eip==0xc0100cb6) printf("0x%x\n",cpu.esp);
 	cpu.eip=swaddr_read(cpu.esp,4,2)-1;
 	if (cpu.eip==0xc0100cb6) printf("0x%x\n",cpu.eip);
 	if (DATA_BYTE==2)
