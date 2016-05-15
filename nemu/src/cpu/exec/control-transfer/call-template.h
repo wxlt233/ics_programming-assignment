93782fbbf886a8a0eb027effc0d654c5b8c675d5
tracer-ics2015 <tracer@njuics.org>
2016-05-15 12:10:40 +0800

    >  compile NEMU 14307130132 litong Linux litong-laptop 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  12:10:40 up  1:07,  4 users,  load average: 0.01, 0.22, 0.19 bce19867c084cd28f0828c0f5e458c117a00c9

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -10,17 +10,41 @@ static void do_execute() {
 		cpu.eip+=(int)(op_src->val);
 		if (DATA_BYTE==2) cpu.eip=cpu.eip&0x0000ffff;
 	} 
-	else {
-		justtry=1;
-		cpu.esp-=4;
-		if (DATA_BYTE==4) swaddr_write(cpu.esp,4,cpu.eip+2,2);
-		cpu.eip=op_src->val;
-		if (DATA_BYTE==2) cpu.eip=cpu.eip&0x0000ffff;
-	}
+//	else {
+//		justtry=1;
+//		cpu.esp-=4;
+//		if (DATA_BYTE==4) swaddr_write(cpu.esp,4,cpu.eip+2,2);
+//		cpu.eip=op_src->val;
+//		if (DATA_BYTE==2) cpu.eip=cpu.eip&0x0000ffff;
+//	}
 	print_asm_template1();
 }
 
 make_instr_helper(i)
-make_instr_helper(rm)
+//make_instr_helper(rm)
+
+#if DATA_BYTE==4
+make_helper(call_rm_l)
+{
+	int len=decode_rm_l(eip+1)+1;
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.eip+len,2);
+	cpu.eip=op_src->val;
+	cpu.eip-=len;
+	return len;
+}
+#endif
+
+#if DATA_BYTE==2
+make_helper(call_rm_w)
+{
+	int len=decode_rm_w(eip+1)+1;
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.eip+len,2);
+	cpu.eip=op_src->val;
+	cpu.eip-=len;
+	return len;
+}
+#endif
 #include "cpu/exec/template-end.h"
 
