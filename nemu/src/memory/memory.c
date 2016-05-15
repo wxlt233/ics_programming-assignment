3be4082bc50bcca4f89a19cd14f7b5dfa230811e
tracer-ics2015 <tracer@njuics.org>
2016-05-15 14:14:56 +0800

    >  compile NEMU 14307130132 litong Linux litong-laptop 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:14:56 up  3:11,  4 users,  load average: 0.11, 0.12, 0.18 a6f11cbd45fc74889c2edf58f7855ba1b0a69d4

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -79,6 +79,7 @@ hwaddr_t  page_translate(lnaddr_t addr)
 {
 	if (cpu.cr0.protect_enable==1&&cpu.cr0.paging==1)
 	{
+		if (cpu.eip==0x8054655) printf("%x\n",cpu.eax);
 		if (hittlb(addr))
 		{
 			uint16_t offset=addr&0xfff;
@@ -89,6 +90,7 @@ hwaddr_t  page_translate(lnaddr_t addr)
 			uint16_t offset=addr&0xfff;
 			uint16_t dir=(addr>>22)&0x3ff;
 			uint16_t page=(addr>>12)&0x3ff;
+
 			PDE aa;
 			aa.val=hwaddr_read((cpu.cr3.page_directory_base<<12)+4*dir,4);
 			if (aa.present==0)
