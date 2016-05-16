e6cc1f0f42d012ceb7ca3f7011ae4e349e417358
tracer-ics2015 <tracer@njuics.org>
2016-05-16 14:06:52 +0800

    >  compile NEMU 14307130132 litong Linux litong-laptop 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:06:52 up  6:21,  5 users,  load average: 0.04, 0.30, 0.33 85af33a7fe658bf89ea1c1d9c575b2621ab1918

--- nemu/src/cpu/exec/string/cmps-template.h
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -6,27 +6,29 @@ make_helper(concat(cmps_m_,SUFFIX))
 {
 	DATA_TYPE src=swaddr_read(cpu.esi,DATA_BYTE,3);
 	DATA_TYPE dest=swaddr_read(cpu.edi,DATA_BYTE,0);
-	DATA_TYPE_S temp=dest+~(src)+1;
+//	DATA_TYPE_S temp=dest+~(src)+1;
+	DATA_TYPE result=src-dest;
+//	DATA_TYPE_S s_dest=(dest>>(DATA_BYTE*8-1))&1;
+//	DATA_TYPE_S s_src=(src>>(DATA_BYTE*8-1))&1;
+//	DATA_TYPE_S s_temp=(temp>>(DATA_BYTE*8-1))&1;
 
-	DATA_TYPE_S s_dest=(dest>>(DATA_BYTE*8-1))&1;
-	DATA_TYPE_S s_src=(src>>(DATA_BYTE*8-1))&1;
-	DATA_TYPE_S s_temp=(temp>>(DATA_BYTE*8-1))&1;
-
-	DATA_TYPE_S dest_and_src=!!(s_dest^s_src);
-	DATA_TYPE_S dest_and_temp=!!(s_dest^s_temp);
-	cpu.eflags.OF=!!(dest_and_src&dest_and_temp);
+//	DATA_TYPE_S dest_and_src=!!(s_dest^s_src);
+//	DATA_TYPE_S dest_and_temp=!!(s_dest^s_temp);
+//	cpu.eflags.OF=!!(dest_and_src&dest_and_temp);
 	
-	cpu.eflags.SF=s_temp;
-
-	if (temp==0) 
+//	cpu.eflags.SF=s_temp;
+	cpu.eflags.CF=(dest<src)?1:0;
+	cpu.eflags.OF=(MSB(dest)^MSB(result))&&(MSB(dest)^MSB(src));
+	cpu.eflags.SF=MSB(result);
+	if (result==0) 
 	{
 		cpu.eflags.ZF=1;
 	}
 	else cpu.eflags.ZF=0;
 
-	if (src<=dest)
-		cpu.eflags.CF=0;
-	else cpu.eflags.CF=1;
+//	if (src<=dest)
+//		cpu.eflags.CF=0;
+//	else cpu.eflags.CF=1;
 
 	if (cpu.eflags.DF==0)
 	{
@@ -39,8 +41,8 @@ make_helper(concat(cmps_m_,SUFFIX))
 		cpu.esi-=DATA_BYTE;
 	}
 	print_asm_template2();
-	return DATA_BYTE+1;
-//	return 1;
+//	return DATA_BYTE+1;
+	return 1;
 }
 #include "cpu/exec/template-end.h"
 
