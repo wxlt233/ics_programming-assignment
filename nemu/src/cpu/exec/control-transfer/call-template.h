d760db74c69a2cbd77c6737b434800d9a546c175
tracer-ics2015 <tracer@njuics.org>
2016-05-16 13:18:17 +0800

    >  compile NEMU 14307130132 litong Linux litong-laptop 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  13:18:17 up  5:33,  5 users,  load average: 0.40, 0.27, 0.28 2184b25937d7298e48b40e2ec920d0708840ffd6

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -8,7 +8,11 @@ static void do_execute() {
 		cpu.esp-=4;
 		
 		if (DATA_BYTE==4)swaddr_write(cpu.esp,4,cpu.eip+5,2);
-		else if (DATA_BYTE==2) swaddr_write(cpu.esp,4,cpu.eip+4,2);
+		else if (DATA_BYTE==2) 
+			{
+				printf("haha");
+				swaddr_write(cpu.esp,4,cpu.eip+4,2);
+			}
 		cpu.eip+=(DATA_TYPE_S)(op_src->val);
 	//	if (DATA_BYTE==2) cpu.eip=cpu.eip&0x0000ffff;
 //	} 
