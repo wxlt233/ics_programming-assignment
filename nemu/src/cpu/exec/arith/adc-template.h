049660430f9f085d5d5a573a1e86aef17bee0c86
tracer-ics2015 <tracer@njuics.org>
2016-05-16 11:03:39 +0800

    >  compile NEMU 14307130132 litong Linux litong-laptop 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  11:03:39 up  3:18,  5 users,  load average: 0.03, 0.14, 0.35 1ee680498cfa786572aaa20fc2bd9f599d63fdd3

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -5,7 +5,7 @@ static void do_execute() {
 	DATA_TYPE result=op_dest->val+op_src->val+cpu.eflags.CF;
     DATA_TYPE a=op_dest->val;
 	DATA_TYPE b=op_src->val;	
-
+	uint64_t tt=(uint64_t)op_dest->val+(uint64_t)op_src->val+cpu.eflags.CF;
 	if (MSB(a)==1&&MSB(b)==1&&MSB(result)==0 ) cpu.eflags.OF=1;
 	else if (MSB(a)==0&&MSB(b)==0&&MSB(result)==1) cpu.eflags.OF=1;
 	else cpu.eflags.OF=0;
@@ -16,11 +16,11 @@ static void do_execute() {
 		cpu.eflags.ZF=0;
 
 	cpu.eflags.SF=MSB(result);
-
-	if (a+b<a||a+b<b) 
-		cpu.eflags.CF=1;
-	else 
-		cpu.eflags.CF=0;
+	cpu.eflags.CF=tt>>((DATA_BYTE)*8)&1;
+//	if (a+b<a||a+b<b) 
+//		cpu.eflags.CF=1;
+//	else 
+//		cpu.eflags.CF=0;
 	short t=(result&1)+((result>>1)&1)+((result>>2)&1)
 		+((result>>3)&1)+((result>>4)&1)+((result>>5)&1)
 		+((result>>6)&1)+((result>>7)&1);
